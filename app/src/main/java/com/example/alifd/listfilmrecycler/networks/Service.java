6e6f93d95c99ce80b4fa0f04ee1432b1100dc7d0
Alif Dewantoro <alif17dewantoro@gmail.com>
2019-10-18 10:49:58 +0700

    update api method

--- app/src/main/java/com/example/alifd/listfilmrecycler/networks/Service.java
+++ app/src/main/java/com/example/alifd/listfilmrecycler/networks/Service.java
@@ -6,31 +6,32 @@ import com.example.alifd.listfilmrecycler.model.TvResponse;
 import io.reactivex.Observable;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface Service {
 
     //@FormUrlEncoded
-    @POST("discover/movie")
+    @GET("discover/movie")
     Observable<FilmResponse> getFilmList(@Query("api_key") String api_key,
                                          @Query("language") String type);
     //@FormUrlEncoded
-    @POST("discover/movie")
+    @GET("discover/movie")
     Observable<FilmResponse> getFilmList(@Query("api_key") String api_key,
                                          @Query("primary_release_date.gte") String date_gte,
                                          @Query("primary_release_date.lte") String date_lte);
     //@FormUrlEncoded
-    @POST("discover/tv")
+    @GET("discover/tv")
     Observable<TvResponse> getTvList(@Query("api_key") String api_key,
                                      @Query("language") String type);
     //@FormUrlEncoded
-    @POST("search/movie")
+    @GET("search/movie")
     Observable<FilmResponse> getFilmListByQuery(@Query("api_key") String api_key,
                                      @Query("language") String type,
                                      @Query("query") String query);
     //@FormUrlEncoded
-    @POST("search/tv")
+    @GET("search/tv")
     Observable<TvResponse> getTvListByQuery(@Query("api_key") String api_key,
                                             @Query("language") String type,
                                             @Query("query") String query);
